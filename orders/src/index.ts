02adf210e8b28a1d0a0745200fd2facbfd5246d6
Steffen Laurens <steffenlaurens@Steffens-MacBook-Air.local>
2020-08-29 19:26:50 +0700

    update start message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,7 +8,7 @@ import { ExpirationCompleteListener } from "./events/listeners/expiration-comple
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
-  console.log("Starting...");
+  console.log("Starting up...");
 
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
