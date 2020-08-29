4e511a7749ccc530244d2fc03e10cf735d6ea048
Steffen Laurens <steffenlaurens@Steffens-MacBook-Air.local>
2020-08-29 16:17:23 +0700

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("Starting up...");
+
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
   if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
